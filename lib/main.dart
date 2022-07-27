c80db08de2fbfc371c30066d7e40308a1134386f
Hatta <hattamuhammadan.hatta@gmail.com>
2022-07-28 04:11:02 +0700

    all commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_application_1/providers/players.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_application_1/page/add_player_page.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
